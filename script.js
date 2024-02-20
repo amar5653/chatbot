d98df5a0f77218a0f8188b64fa1a78582ca3d738
Amarpreet <gillpreet1998@gmail.com>
2024-02-20 16:50:33 -0700

    script

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
+require('dotenv').config();
+
 const sendChatBtn = document.querySelector(".chat-input span");
 const chatInput = document.querySelector(".chat-input textarea");
 const chatbox = document.querySelector(".chatbox");
 
 let userMessage;
+const API_KEY = process.env.API_KEY;
+
+const generateResponse = () => {
+    const API_URL = "https://api.openai.com/v1/chat/completions";
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+  "Content-Type": "application/json" ,
+  "Authorization": `Bearer ${API_URL}` 
+        },
+        body: JSON.stringify({
+    model: "gpt-3.5-turbo",
+    messages: 
+    [
+        {role: "user", content: userMessage}
+    ]
+    })
+    }
+
+    //Send POST request to API, get response
+    fetch(API_URL, requestOptions).then(res=> res.json()).then(data=> {
+        console.log(data);
+    }).catch((error)=> {
+        console.log(error);
+    })
+}
 
 const createChatLi = (message, className) => {
     
@@ -23,8 +52,11 @@ const handleChat = () => {
   //Append the user's message to the chatbox
   chatbox.appendChild(createChatLi(userMessage, "outgoing"));
 
- chatOutgoing.innerHTML = userMessage;
-
+ setTimeout(() => {
+    //Display thinking message while waiting for the response
+    chatbox.appendChild(createChatLi("Thinking...","incoming"));
+ }, 600);
+     generateResponse();
 }
 
 sendChatBtn.addEventListener("click", handleChat);
